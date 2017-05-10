6d4168adb58517327a01fde44b5fa0a3dd5567a2
JamesRMJackson <j.rmjackson@hotmail.com>
2017-05-10 12:37:01 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Coding For Nuttersons</title>
+<meta charset="UFT-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/style.css">
+
+<style>
+	
+</style>
+</head>
+
+<body>
+<!-- Main content-->
+<div class="container">
+<?php include('include/topheader.html');?>
+
+
+<?php include('include/middleheader.html');?>
+<!-- body content -->
+
+<div class="menu-banner" style="background-image: url(images/banner-960-130.jpg)";>
+<div class="contain">
+<div class="banner-show-text">
+	<h1 class="title">Insight &amp; Knowledge</h1>
+	<p class="banner-s-text">Your Centre for advice, know-how, and the latest legal development.</p>
+</div>
+
+</div>
+</div>
+<div class="menu-header">
+<?php include('include/menutop.html');?>
+</div>
+
+
+<div class="col-5 md pd">
+<h1 class="title pt">Coding for nuttersons</h1>
+<div class="mid-news">
+<img src="images/Corporate-ClydeCo-0010_552_382_90_s_c1_c_c.jpg" alt="Nutterson" height="400px" width="100%">
+<hr>
+<h4 class="title">27 April, 2017</h4>
+<p><span style="text-transform: uppercase;">circ</span> calls for comments on <span class="text-cap">draft changes</span> to Administrative Regulations for Foreign-Investment Insurers</p>
+</div>
+</div>
+<div class="col-5 md pd">
+	<div class="mid-new">
+	<ol class="updates">
+		<li><a href="#">
+		<div class="date-img"><img src="images/Corporate-ClydeCo-0010_552_382_90_s_c1_c_c.jpg" alt="Nutterson" height="80" width="80"></div>
+		<div class="update-text">
+			<p class="date"></p>
+			<h3 class="date-head">CIRC - Risk Prevention Notice (Notice) (Chinese)</h3>
+			</div>
+		</a>
+		</li>
+		<li><a href="#">
+		<div class="date-img"><img src="images/Corporate-ClydeCo-0010_552_382_90_s_c1_c_c.jpg" alt="Nutterson" height="80" width="80"></div>
+		<div class="update-text">
+			<p class="date"></p>
+			<h3 class="date-head">CIRC - Risk Prevention Notice (Notice) (Chinese)</h3>
+			</div>
+		</a>
+		</li>
+		<li><a href="#">
+		<div class="date-img"><img src="images/Corporate-ClydeCo-0010_552_382_90_s_c1_c_c.jpg" alt="Nutterson" height="80" width="80"></div>
+		<div class="update-text">
+			<p class="date"></p>
+			<h3 class="date-head">CIRC - Risk Prevention Notice (Notice) (Chinese)</h3>
+			</div>
+		</a>
+		</li>
+		<li><a href="#">
+		<div class="date-img"><img src="images/Corporate-ClydeCo-0010_552_382_90_s_c1_c_c.jpg" alt="Nutterson" height="80" width="80"></div>
+			<div class="update-text">
+			<p class="date"></p>
+			<h3 class="date-head">CIRC - Risk Prevention Notice (Notice) (Chinese)</h3>
+	    </div>
+	    </a>
+		</li>
+</div>
+</div>
+
+<!-- footer -->
+<?php include('include/footer.html');?>
+
+
+</body>
+</html>
